418f5f21ee30b0310aa951e4575e454ba44da8cd
Leo Lu <leo@LEOPC.eomr.local>
2015-07-08 21:20:51 +0800

    fix

--- R/get_panel_id.R
+++ R/get_panel_id.R
@@ -124,19 +124,20 @@ sample_N <- function(.data, .n, .id_var="Panel_id",
 
 n_sent_id <- function (sent_id_path, date_from=NULL, date_to=NULL) {
   
-  sent_id_path <- normalizePath(sent_id_path)
+  sent_id_path <- suppressWarnings(normalizePath(sent_id_path))
   
   if(!is.null(date_from) & !is.null(date_to)) {
     time_interval <- seq(as.Date(date_from), as.Date(date_to), "day") %>%
       as.character %>%
-      paste0(collapse="|")
+      paste0(".*\\.txt", collapse="|")
     
     list.files(path = sent_id_path,
-               pattern = time_interval, full.names=TRUE) %>%
+               pattern = time_interval,
+               full.names=TRUE) %>%
       plyr::llply(readr::read_table,
                   col_names = F,
                   col_types = "c") %>%
-      unlist %>% unname %>% length %>% unique %>%
+      unlist %>% unname %>%  unique %>% length %>%
       cat(date_from, "至", date_to, "期間共發送了\n", ., "個ID")
   } else {
     list.files(path = sent_id_path,
