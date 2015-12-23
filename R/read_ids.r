0ef7f358454d27d259b2e866e4fbe4280155fd14
Leo Lu <leo@LEOPC.eomr.local>
2015-12-23 14:30:56 +0800

    fix bug

--- R/read_ids.r
+++ R/read_ids.r
@@ -1,7 +1,7 @@
 #' Read IDs from single txt file path, or txt files in directory
 #'
 #' @param path txt file path, or directory contains txt files.
-#' @param pattern Character string containing a regular expression 
+#' @param pattern Character string containing a regular expression
 #'        to be matched in the given character vector. Only work for dir path.
 #' @param show_path Print file path or not. Only work for dir path.
 #'
@@ -11,7 +11,7 @@
 #' @export
 read_ids <- function(path, pattern = ".*", show_path = FALSE) {
   path <- check_dir_file(path)
-  
+
   switch(path_format(path),
          txt =  read_ids_txt(path),
          dir_path = read_ids_path(path, pattern, show_path))
@@ -21,7 +21,7 @@ read_ids_txt <- function(path) {
   file_path <- normalizePath(path,
                              winslash = "/",
                              mustWork = TRUE)
-  
+
   ids <- readLines(path, skipNul = TRUE)
   ids <- unname(unlist(ids))
   ids
@@ -35,21 +35,21 @@ read_ids_path <- function(path, pattern, show_path) {
     include.dirs = FALSE,
     full.names = TRUE
   )  # list of file path
-  
-  path_dir <- dirname(file_path)
+
+  path_dir <- path
   path_file <- grep(pattern, basename(file_path), value = TRUE)
-  
+
   file_path <- normalizePath(file.path(path_dir, path_file),
                              winslash = "/",
                              mustWork = TRUE)
-  
+
   if (show_path) {
     cat("read files from:", file_path, "\n", sep = "\n")
   }
-  
+
   ids <- plyr::llply(file_path, readLines, skipNul = TRUE)
-  ids <- unlist(ids, use.names = FALSE)
-  invisible(ids)
+  ids <- unique(unlist(ids, use.names = FALSE))
+  ids
 }
 
 
@@ -59,7 +59,7 @@ path_format <- function (path) {
   ext <- tolower(tools::file_ext(path))
   if (ext == "" && is_dir(path))
     ext <- "dir_path"
-  
+
   switch (ext,
           txt = "txt",
           dir_path = "dir_path",
