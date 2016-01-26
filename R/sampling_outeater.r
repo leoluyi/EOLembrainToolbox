3b53034cc583510b9a6a381265c5c3e912ee7106
Leo Lu <leo0650@gmail.com>
2016-01-26 11:09:32 +0800

    update

--- R/sampling_outeater.r
+++ R/sampling_outeater.r
@@ -1,12 +1,12 @@
 #' Outeater Sampling
 #'
-#' @param panel_id 
-#' @param sampleN 
-#' @param sent_id 
-#' @param finished_id 
+#' @param panel_id
+#' @param sampleN
+#' @param sent_id
+#' @param finished_id
 #'
 #' @return character vector
-#' @export 
+#' @export
 #'
 #' @examples
 #' sampleN <- list(
@@ -19,18 +19,18 @@
 #' KS_PT = list(N = 100, age = c(15:20, 35:59), gender=1:2, include_sent=F),
 #' HWL_TD = list(N = 20, age = c(15:49), gender=1:2, include_sent=T)
 #' )
-#' 
+#'
 #' id_output <- panel_id_all %>%
 #' filter(active_grade %in% 1:2) %>%  # active, semi-active
 #'   sampling_outeater(sampleN,
 #'                     sent_id,
 #'                     finished_id)
-#' 
-#' 
+#'
+#'
 sampling_outeater <- function (panel_id=panel_id_active, sampleN,
                                sent_id = NULL,
                                finished_id = NULL) {
-  
+
   cat("\u53f0\u5317:\t")
   id_TP <- panel_id %>%
     filter(aream_name %in% c("\u81fa\u5317\u5e02")) %>%           # city
@@ -41,7 +41,7 @@ sampling_outeater <- function (panel_id=panel_id_active, sampleN,
              finished_id = finished_id,
              include_sent = sampleN$TP$include_sent,
              show = T)    # Sample N id
-  
+
   cat("\u65b0\u5317:\t")
   id_NewTP <- panel_id %>%
     filter(aream_name %in% c("\u65b0\u5317\u5e02")) %>%            # city
@@ -52,7 +52,7 @@ sampling_outeater <- function (panel_id=panel_id_active, sampleN,
              finished_id = finished_id,
              include_sent = sampleN$NewTP$include_sent,
              show = T)     # Sample N id
-  
+
   cat("\u57fa\u5b9c:\t")
   id_KL_YL <- panel_id %>%
     filter(aream_name %in% c("\u57fa\u9686\u5e02", "\u5b9c\u862d\u7e23")) %>% # city
@@ -63,7 +63,7 @@ sampling_outeater <- function (panel_id=panel_id_active, sampleN,
              finished_id = finished_id,
              include_sent = sampleN$KL_YL$include_sent,
              show = T)   # Sample N id
-  
+
   cat("\u6843\u7af9\u82d7:\t")
   id_TY_XC_ML <- panel_id %>%
     filter(aream_name %in% c("\u6843\u5712\u5e02", "\u65b0\u7af9\u5e02",
@@ -75,7 +75,7 @@ sampling_outeater <- function (panel_id=panel_id_active, sampleN,
              finished_id = finished_id,
              include_sent = sampleN$TY_XC_ML$include_sent,
              show = T)   # Sample N id
-  
+
   cat("\u4e2d\u5f70\u6295:\t")
   id_TCH_CHW_NT <- panel_id %>%
     filter(aream_name %in% c("\u81fa\u4e2d\u5e02", "\u5f70\u5316\u7e23", "\u5357\u6295\u7e23")) %>%  # city
@@ -86,10 +86,10 @@ sampling_outeater <- function (panel_id=panel_id_active, sampleN,
              finished_id = finished_id,
              include_sent = sampleN$TCH_CHW_NT$include_sent,
              show = T)  # Sample N id
-  
+
   cat("\u96f2\u5609\u5357:\t")
   id_YL_CHY_TN <- panel_id %>%
-    filter(aream_name %in% c("\u96f2\u6797\u7e23", "\u5609\u7fa9\u5e02", 
+    filter(aream_name %in% c("\u96f2\u6797\u7e23", "\u5609\u7fa9\u5e02",
                              "\u5609\u7fa9\u7e2","\u81fa\u5357\u5e02")) %>% # city
     filter(gender %in% sampleN$YL_CHY_TN[["gender"]]) %>%    # gender
     filter(age %in% sampleN$YL_CHY_TN[["age"]]) %>%   # age
@@ -98,7 +98,7 @@ sampling_outeater <- function (panel_id=panel_id_active, sampleN,
              finished_id = finished_id,
              include_sent = sampleN$YL_CHY_TN$include_sent,
              show = T)    # Sample N id
-  
+
   cat("\u9ad8\u5c4f:\t")
   id_KS_PT <- panel_id %>%
     filter(aream_name %in% c("\u9ad8\u96c4\u5e02", "\u5c4f\u6771\u7e23")) %>% # city
@@ -109,7 +109,7 @@ sampling_outeater <- function (panel_id=panel_id_active, sampleN,
              finished_id = finished_id,
              include_sent = sampleN$KS_PT$include_sent,
              show = T)     # Sample N id
-  
+
   cat("\u82b1\u6771:\t")
   id_HWL_TD <- panel_id %>%
     filter(aream_name %in% c("\u82b1\u84ee\u7e23", "\u81fa\u6771\u7e2")) %>% # city
@@ -120,7 +120,7 @@ sampling_outeater <- function (panel_id=panel_id_active, sampleN,
              finished_id = finished_id,
              include_sent = sampleN$HWL_TD$include_sent,
              show = T)    # Sample N id
-  
+
   id_output <- c(id_TP,
                  id_NewTP,
                  id_KL_YL,
@@ -129,4 +129,5 @@ sampling_outeater <- function (panel_id=panel_id_active, sampleN,
                  id_YL_CHY_TN,
                  id_KS_PT,
                  id_HWL_TD)
+  id_output
 }
